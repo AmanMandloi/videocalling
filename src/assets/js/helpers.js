60c0abd2221257fc9fc3cf5cb289c55a158697cb
Aman Mandloi <36765530+AmanMandloi@users.noreply.github.com>
2020-07-06 00:00:50 +0530

    Update helpers.js

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -109,19 +109,21 @@ export default {
 
     getIceServer() {
         return {
-            iceServers: [
-                {
-                    urls: ["stun:eu-turn4.xirsys.com"]
-                },
-                {
-                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
-                    credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
-                    urls: [
-                        "turn:eu-turn4.xirsys.com:80?transport=udp",
-                        "turn:eu-turn4.xirsys.com:3478?transport=tcp"
-                    ]
-                }
-            ]
+iceServers: [{
+   urls: [ "stun:bn-turn1.xirsys.com" ]
+}, {
+   username: "n_cflXuHwPwdKY1XtF88qbMN2nQdsLabXrCtp8PaP81-PDhuFtGsVJ5bD8sVhgn2AAAAAF8CG2FweXRob25tYXRleW9nZXNo",
+   credential: "1270a350-beed-11ea-8a34-0242ac140004",
+   urls: [
+       "turn:bn-turn1.xirsys.com:80?transport=udp",
+       "turn:bn-turn1.xirsys.com:3478?transport=udp",
+       "turn:bn-turn1.xirsys.com:80?transport=tcp",
+       "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+       "turns:bn-turn1.xirsys.com:443?transport=tcp",
+       "turns:bn-turn1.xirsys.com:5349?transport=tcp"
+   ]
+}]
+            
         };
     },
 
