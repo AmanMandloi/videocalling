68c32650a64b9192fb9bd335e0b5503e73e38a7c
Aman Mandloi <36765530+AmanMandloi@users.noreply.github.com>
2020-07-05 23:49:34 +0530

    Update helpers.js

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -111,14 +111,14 @@ export default {
         return {
             iceServers: [
                 {
-                    urls: ["stun:eu-turn4.xirsys.com"]
+                    urls: ["stun:http://ec2-13-232-58-108.ap-south-1.compute.amazonaws.com:3000/"]
                 },
                 {
-                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
-                    credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
+                    username: "pythonmateyogesh",
+                    credential: "10f5d14c-beea-11ea-b03b-0242ac150002",
                     urls: [
-                        "turn:eu-turn4.xirsys.com:80?transport=udp",
-                        "turn:eu-turn4.xirsys.com:3478?transport=tcp"
+                        "turn:http://ec2-13-232-58-108.ap-south-1.compute.amazonaws.com:3000/?transport=udp",
+                        "turn:http://ec2-13-232-58-108.ap-south-1.compute.amazonaws.com:3000/?transport=tcp"
                     ]
                 }
             ]
